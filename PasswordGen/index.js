1bff627a48b875ad67935770d95490ad3de016a1
Anupta <anupta22@gmail.com>
2024-06-24 21:36:21 -0400

    Password Generator is fully functional and looks good

--- PasswordGen/index.js
+++ PasswordGen/index.js
@@ -1,5 +1,3 @@
-// ([]$&+,:;=?@#|'<>.-^*()%!)
-
 /* range slider variables */
 const slider = document.getElementById("len-slider");
 const length = document.getElementById("current-length");
@@ -19,7 +17,7 @@ const generate = document.getElementsByClassName("generate")[0];
 const pass_cont = document.querySelector(".pass-cont");
 const copy_btn = pass_cont.querySelector(".copy-button");
 const input_field = pass_cont.querySelector(".copy-link-input");
-const text = input_field.value;
+var text = input_field.value;
 
 var password;
 
@@ -34,28 +32,72 @@ function generatePassword() {
     return;
   password = "";
 
-  var remainChars =
-    parseInt(slider.value) -
-    (lowercase +
-      (uppercase.checked ? 1 : 0) +
-      (number.checked ? 1 : 0) +
-      (special.checked ? 1 : 0));
+  const alphabet_chars = "abcdefghijklmnopqrstuvwxyz";
+  const numerical_chars = "0123456789";
+  const special_chars = "[]$&+,:;=?@#|'<>.-^*()%!";
+  const space_char = " ";
 
-  var numLower = 0;
-  var numUpper = 0;
-  var numNumber = 0;
-  var numSpecial = 0;
-  var numSpace = 0;
+  var numLower = lowercase.checked ? 1 : 0;
+  var numUpper = uppercase.checked ? 1 : 0;
+  var numNumber = number.checked ? 1 : 0;
+  var numSpecial = special.checked ? 1 : 0;
+  var numSpace = space.checked ? 1 : 0;
 
-  if (lowercase.checked) {
-    remainChars -=
-      (uppercase.checked ? 1 : 0) +
-      (number.checked ? 1 : 0) +
-      (special.checked ? 1 : 0);
-    numLower = Math.floor(1 + Math.random() * remainChars);
+  var chars = [numLower, numUpper, numNumber, numSpecial, numSpace];
+
+  var remainChars = parseInt(slider.value);
+  for (let i = 0; i < chars.length; i++) remainChars -= chars[i];
+
+  let idx;
+  while (remainChars != 0) {
+    idx = Math.floor(Math.random() * chars.length);
+    if (chars[idx] != 0) {
+      chars[idx]++;
+      remainChars--;
+    }
   }
 
-  console.log(numLower);
+  remainChars = parseInt(slider.value);
+
+  while (remainChars != 0) {
+    let nextChar;
+    idx = Math.floor(Math.random() * chars.length);
+
+    if (chars[idx] != 0) {
+      switch (idx) {
+        case 0:
+          nextChar = alphabet_chars.charAt(
+            Math.floor(Math.random() * alphabet_chars.length)
+          );
+          break;
+        case 1:
+          nextChar = alphabet_chars
+            .charAt(Math.floor(Math.random() * alphabet_chars.length))
+            .toUpperCase();
+          break;
+        case 2:
+          nextChar = numerical_chars.charAt(
+            Math.floor(Math.random() * numerical_chars.length)
+          );
+          break;
+        case 3:
+          nextChar = special_chars.charAt(
+            Math.floor(Math.random() * special_chars.length)
+          );
+          break;
+        case 4:
+          nextChar = space_char;
+      }
+
+      if (duplicate.checked || !password.includes(nextChar)) {
+        password += nextChar;
+        chars[idx]--;
+        remainChars--;
+      }
+    }
+  }
+  text = password;
+  input_field.value = text;
 }
 
 generate.addEventListener("click", generatePassword);
